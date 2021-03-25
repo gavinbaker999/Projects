4e898758c6c2e076ced9d08351917e418f4452be
gavinbaker999 <endhousesoftware999@gmail.com>
2021-03-25 07:37:52 +0000

    Import jokes.json into mysql db as csv format

--- fathom/joker/public/dbcreate.sql
+++ fathom/joker/public/dbcreate.sql
@@ -1,7 +1,18 @@
 CREATE DATABASE IF NOT EXISTS jokes;
 USE jokes;
-CREATE TABLE IF NOT EXISTS jsontest(
-     rowid INT AUTO_INCREMENT NOT NULL UNIQUE,
-     jsondata json
+DROP TABLE IF EXISTS jokedata;
+CREATE TABLE jokedata(
+     jokeId INT NOT NULL UNIQUE,
+     jokeType VARCHAR(50) NOT NULL,
+     jokeSetup VARCHAR(200) NOT NULL,
+     jokePunchline VARCHAR(200) NOT NULL,
+     PRIMARY KEY (jokeId)
      );
-LOAD DATA LOCAL INFILE 'jokes.json' into table jsontest(jsondata);
+LOAD DATA INFILE 'jokes.csv' 
+  INTO TABLE discounts 
+  FIELDS TERMINATED BY ',' 
+  ENCLOSED BY '"'
+  LINES TERMINATED BY '\n'
+  IGNORE 1 ROWS
+  (@jokeId,jokeType,jokeSetup,jokePunchLine)
+  SET jokeId = CAST(@jokeId AS UNSIGNED);
