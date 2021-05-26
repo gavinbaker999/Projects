640185a48f677c4bac0a4cff5af50d2a615912eb
gavinbaker999 <endhousesoftware999@gmail.com>
2021-05-26 08:56:43 +0100

    Starting on CallGraphNode.pm

--- perl/callgraphnode.pm
+++ perl/callgraphnode.pm
@@ -1,29 +1,20 @@
 package CallGraphNode;
-use ObjectTemplate;
 require Exporter;
-@ISA = qw(ObjectTemplate Exporter);
+@ISA = qw(Exporter);
+@EXPORT = qw(funca);
 $VERSION = 1.0;
 
-attributes('filename','classname','fuctionname','linenumber','funcscalled');
-
-# initialize will be called by new()
-sub initialize {
-#    my $self = shift;
-#    $self->three(1) unless defined $self->three();
+sub new {
+    my $class = shift;
+    my $self = {};
+    bless $self,$class;
+    return $self;
 }
 
-#sub create {
-#	my ($pkg,$name) = @_;
-#	return {bless {name => $name}, $pkg};
-#}
-#sub DESTROY {
-#	my $obj = shift;
-#}
-#
-#sub name {
-#	my $obj = shift;
-#	@_ ? $obj->{name} = shift;
-#	   : $obj->{name};
-#}
+sub funca {
 
+}
 
+sub DESTROY {
+    
+}
